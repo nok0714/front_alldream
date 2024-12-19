9541e1bab393aef55760c2dc279c4c8d3272c5a0
Nok0714 <79920407+nok0714@users.noreply.github.com>
2024-12-20 00:20:48 +0900

    Update now.js

--- now.js
+++ now.js
@@ -18,8 +18,6 @@ socket.onclose = (event) => {
     console.log("연결 끊어짐");
     teamContainer.innerHTML = "서버와의 연결이 끊어졌습니다"
     numContainer.innerHTML = "새로고침 해주세요!"
-    optionButton.innerHTML = "서버와의 연결이 끊어졌습니다"
-    optionButton.disabled = true
 };
 
 
@@ -28,13 +26,9 @@ function updateResults(results) {
     if (results[0] == "대기 중"){
         teamContainer.innerHTML = "현재 투표 대기 중입니다"
         numContainer.innerHTML = "공연을 기다리는 중 ^_^"
-        optionButton.innerHTML = "투표 대기중"
-        optionButton.disabled = true
     }
     else {
         teamContainer.innerHTML = "현재 투표중 : " + results[0];
         numContainer.innerHTML = results[1] + "표";
-        optionButton.disabled = false
-        optionButton.innerHTML = "투표하기"
     }
 }
